52f7921b5fade905d5fdc4042e2e177919f1dee4
ChenYouXin8 <154851375+ChenYouXin8@users.noreply.github.com>
2026-08-18 21:29:20 +0800

    feat: 初始化 Spring Boot 用户中心项目- Spring Boot 4.0.7 - Java 21 -引入 MyBatis 依赖 - 包路径: io.github.chenyouxin8.usercenter

--- src/main/java/io/github/chenyouxin8/usercenter/UserCenterApplication.java
+++ src/main/java/io/github/chenyouxin8/usercenter/UserCenterApplication.java
@@ -1,11 +1,12 @@
 package io.github.chenyouxin8.usercenter;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("io.github.chenyouxin8.usercenter.mapper")  // 指定Mapper接口的包路径
 public class UserCenterApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserCenterApplication.class, args);
     }
